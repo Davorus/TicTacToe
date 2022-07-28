1a447657c1124b7ab05c8ea7943103a5809a0c25
Davorus <67279830+Davorus@users.noreply.github.com>
2022-07-28 09:37:56 +0200

    Utilities um Input erweitert

--- Utilities.hpp
+++ Utilities.hpp
@@ -6,9 +6,16 @@
 namespace MyUtils
 {
     template <class T>
-    static void print(T p_input)
+    static void print(T &p_input)
     {
         std::cout << p_input << std::endl;
+    }
+
+    template <class T>
+    static void input(T &input)
+    {
+        print("Enter value:");
+        std::cin >> input;
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include "Game_Handler.hpp"
 #include "Gamesetup_Handler.hpp"
 #include "Utilities.hpp"
-#include "Playfield_Handler.hpp"
 
 int main()
 {
+    
     Game_Handler gh;
     Gamesetup_Handler h(gh);
     h.print_Start_Menu();
